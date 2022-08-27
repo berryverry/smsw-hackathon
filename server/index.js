f3163040b4a42bea74897d7c5317d46dd6cc58dd
lucy <lucy1287@naver.com>
2022-08-27 23:47:45 +0900

    board, comment write

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 const { User } = require('./models/User');
+const { Board } = require('./models/Board');
+const { Comment } = require('./models/Comment');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -72,6 +74,64 @@ app.post('/login', (req, res) => {
           });
       })
       .catch((err) => res.json({ loginSuccess: false, err }));
+  });
+});
+
+
+//게시글 작성
+app.post('/board/write', function (req, res) {
+  let board = new Board();
+  board.title = req.body.title;
+  board.contents = req.body.contents;
+  board.author = req.body.author;
+
+  board.save(function (err) {
+    if(err){
+      console.log(err);
+      res.redirect('/');
+    }
+    res.redirect('/');
+  });
+});
+
+//게시글 리스트 보기
+app.get('/board/list', function(req, res){
+  let posts = Board.find( function(err, posts){
+    if(err){
+      return res.status(400).json({
+        boardListSuccess: false,
+        message: '게시글 검색에 실패하였습니다.',
+      });
+    }
+    return res.status(200).json({ posts });
+  })
+});
+
+//seq로 게시글 상세보기
+app.get('/board/:seq', function (req, res) {
+  Board.findOne({seq: req.params.seq}, function (err, board) {
+    if (err) {
+      return res.status(400).json({
+        boardSuccess: false,
+        message: '존재하지 않는 글입니다.',
+      });
+    }
+    return res.status(200).json({ board });
+  })
+});
+
+//댓글 작성
+app.post('/comment/:seq/write', function (req, res){
+  const comment = new Comment();
+  comment.contents = req.body.contents;
+  comment.author = req.body.author;
+
+  Board.findOneAndUpdate({seq : req.params.seq}, { $push: { comments : comment}}, function (err, board) {
+    if(err){
+      console.log(err);
+      res.redirect('/');
+    }
+    res.redirect('/board/'+req.params.seq);
   });
 });
 
